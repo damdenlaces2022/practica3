601528f7ab6288b5c49edb585d0503d5ffc65080
Kuba <kubapawelmlynarczyk@gmail.com>
2023-01-13 13:35:37 +0100

    incio java

---
+++ src/proyecto/ed/ProyectoED.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.ed;
+
+/**
+ *
+ * @author usuario
+ */
+public class ProyectoED {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Hola mundo
+        System.out.println("Hello World!");
+    }
+    
+}
